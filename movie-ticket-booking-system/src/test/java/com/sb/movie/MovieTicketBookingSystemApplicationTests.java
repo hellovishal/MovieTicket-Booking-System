225b7522567eac2acfe4d9be8aca0752f27304e4
Vishal Rajak <rvrajvishalqwer@gmail.com>
2026-07-24 13:02:36 +0530

    Adding Unit Tests

---
+++ movie-ticket-booking-system/src/test/java/com/sb/movie/MovieTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
