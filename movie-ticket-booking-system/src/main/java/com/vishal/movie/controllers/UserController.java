aa8849be23b64fa32c5cee177ccde05de020e07c
Vishal Rajak <rvrajvishalqwer@gmail.com>
2026-07-24 12:53:34 +0530

    Adding controllers and custom exception layer

---
+++ movie-ticket-booking-system/src/main/java/com/vishal/movie/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.sb.movie.controllers;
+
+import com.sb.movie.request.UserRequest;
+import com.sb.movie.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/addNew")
+    public ResponseEntity<String> addNewUser(@RequestBody UserRequest userEntryDto) {
+        try {
+            String result = userService.addUser(userEntryDto);
+            return new ResponseEntity<>(result, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
